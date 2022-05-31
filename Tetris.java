010293440454ec20297c7e106fe16f037d141b71
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-31 10:59:11 +0100

    Update Tetris.java

--- Tetris.java
+++ Tetris.java
@@ -1,7 +1,34 @@
 package com.codegym.task.task22.task2213;
 
 public class Tetris {
+    private Field field;
+    private GamePiece gamePiece;
+
     public static void main(String[] args) {
         
+    }
+
+    public void run(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public GamePiece getGamePiece() {
+        return gamePiece;
+    }
+
+    public void setGamePiece(GamePiece gamePiece) {
+        this.gamePiece = gamePiece;
     }
 }
