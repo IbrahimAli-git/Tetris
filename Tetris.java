1ad51a46c4eb78d72275023f27fadfe04da117dc
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-31 11:10:03 +0100

    Update Tetris.java

--- Tetris.java
+++ Tetris.java
@@ -3,16 +3,18 @@ package com.codegym.task.task22.task2213;
 public class Tetris {
     private Field field;
     private GamePiece gamePiece;
+    static Tetris game;
 
     public static void main(String[] args) {
-        
+        game = new Tetris();
+        game.run();
     }
 
     public void run(){
 
     }
 
-    public void stop(){
+    public void step(){
 
     }
 
